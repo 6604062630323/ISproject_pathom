c61358653e1a8d00c9600a98cb7293411716b3d3
6604062630323 <s6604062630323@email.kmutnb.ac.th>
2025-03-16 18:25:16 +0700

    fix

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ st.write("""This is website for **Intelligence System Project**
 In the website, the content includes the following:  
 • **Machine learning detail** (House prediction at Hogwarts)  
 • **Machine learning demo**   (House prediction at Hogwarts)  
-• **Machine learning demo**   (Weather Image) 
-• **Machine learning demo**   (Weather Image) 
+• **neural network demo**   (Weather Image) 
+• **neural network demo**   (Weather Image) 
 
 จัดทำโดย นายปฐมพร ศรีคชพงษ์ 6604062630323  
 """)
